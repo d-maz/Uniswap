2f7b2f11214b60ce78102dc8ccc849f3a91e7c97
d-maz <86002944+d-maz@users.noreply.github.com>
2021-07-11 20:25:59 +1000

    update to daily fees

--- Code/pool_analysis.py
+++ Code/pool_analysis.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import requests
 import json
 import pandas as pd
-from liquidity import calc_liquidity
+from liq_library import calc_liquidity
 
 url = 'https://api.flipsidecrypto.com/api/v2/queries/eaad960d-2c48-4f95-abbf-51da8197689e/data/latest'
 
@@ -18,8 +18,8 @@ pool_df = pd.DataFrame(response.json())
 
 #pool_df['PRICE'] = pool_df[['TOKEN1_ADDRESS']].applymap(lambda x: price_df[price_df['TOKEN_ADDRESS'] == x]['PRICE'].values) 
 
-usd_quant = 10000
-variance = 0.1
+usd_quant = 5000
+variance = 0.2
 
 pool_df['XREAL'] = usd_quant/pool_df[['TOKEN1_ADDRESS']].applymap(lambda x: price_df[price_df['TOKEN_ADDRESS'] == x]['PRICE'].values) 
 
@@ -36,7 +36,16 @@ pool_df = pool_df[pool_df['XREAL'].map(len) == 1]
 
 pool_df['LIQUID'] = pool_df.apply(lambda x : calc_liquidity(x['PRICE'], x['UPPER_PRICE'], x['LOWER_PRICE'], x['XREAL'], x), axis=1)
 
-print([pool_df['POOL_NAME'], pool_df['LIQUID']])
+pool_df['OWNERSHIP'] = pool_df['LIQUID'].values/(pool_df['ACTIVE_LIQUIDITY'].values+pool_df['LIQUID'].values)
+
+pool_df['DAILY_FEES'] = pool_df['OWNERSHIP'].values*pool_df['MAX(DAY_VOL_USD)'].values*pool_df['MAX(FEE_PERCENT)'].values/100
+pool_df['WEEKLY_FEES'] = pool_df['OWNERSHIP'].values*pool_df['MAX(WEEK_VOL_USD)'].values*pool_df['MAX(FEE_PERCENT)'].values/100
+fee_df = pool_df[['POOL_NAME','DAILY_FEES', 'WEEKLY_FEES', 'OWNERSHIP']]#.sort_values('DAILY_FEES', ascending=False)
+fee_df['DAILY_FEES'] = fee_df[['DAILY_FEES']].applymap(lambda x : np.round(np.asscalar(x),2))
+fee_df['WEEKLY_FEES'] = fee_df[['WEEKLY_FEES']].applymap(lambda x : np.round(np.asscalar(x),2))
+
+fee_df = fee_df[fee_df['DAILY_FEES'].notna()].sort_values('DAILY_FEES', ascending=False)
+
 
 
 
