949c5650fed4c579ae0f4e6cbf9646033001a615
d-maz <86002944+d-maz@users.noreply.github.com>
2021-07-28 22:50:48 +1000

    updates

--- Code/fee_estimation.py
+++ Code/fee_estimation.py
@@ -12,6 +12,8 @@ upper_price = current_price*1.2
 lower_price = current_price*1/1.2
 real_quantity = 6
 
+
+
 Liquidity = calc_liquidity(current_price,upper_price,lower_price,real_quantity)
 #print(type(Liquidity), type(USDC_required))
 
@@ -25,7 +27,11 @@ pool_address = ['0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640', #usdc eth 0.05
     '0x11b815efb8f581194ae79006d24e0d814b7697f6' #usdt eth 0.05
     ] #
 
-#pool_address = ['0xcbcdf9626bc03e24f779434178a73a0b4bad62ed']
+
+pool_address = ['0x4585fe77225b41b697c938b018e2ac67ac5a20c0']
+#'0xcbcdf9626bc03e24f779434178a73a0b4bad62ed']
+
+pool_address = ['0x8ecc2244e67d0bb6a1850b1db825e25354cf881a']
 url = 'https://api.flipsidecrypto.com/api/v2/queries/79d15dfa-8714-4256-86b1-ae4dfd37b747/data/latest'
 
 response = requests.get(url)
@@ -43,3 +49,23 @@ for index, value in enumerate(pool_ownership):
     print('Estimated daily fees is $', round(value*daily_fees[index],2))
     print('Estimated weekly fees is $', round(value*weekly_fees[index],2))
 
+
+spread = [1.05, 1.08, 1.09, 1.1, 1.15, 1.2]
+
+for value2 in spread:
+    current_price = 1910
+    upper_price = current_price*value2
+    lower_price = current_price/value2
+    real_quantity = 1
+
+    Liquidity = calc_liquidity(current_price,upper_price,lower_price,real_quantity)
+
+    pool_ownership = Liquidity[0]/((np.array(sdf['ACTIVE_LIQUIDITY'])) + Liquidity[0])
+    daily_fees = np.array(sdf['MAX(FEE_PERCENT)']) / 100 * np.array(sdf['MAX(DAY_VOL_USD)'])
+    weekly_fees = np.array(sdf['MAX(FEE_PERCENT)']) / 100 * np.array(sdf['MAX(WEEK_VOL_USD)'])
+
+    for index, value in enumerate(pool_ownership):
+        print(index, value2, round(lower_price,2), round(upper_price,2))
+        print('Estimated pool share in', sdf["POOL_NAME"][index], 'is:', round(value*100,3), '%')
+        print('Estimated daily fees is $', round(value*daily_fees[index],2))
+        print('Estimated weekly fees is $', round(value*weekly_fees[index],2))
--- Code/risk_metric.py
+++ Code/risk_metric.py
@@ -8,6 +8,7 @@ import random
 
 pool = '0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8'
 #pool = '0xcbcdf9626bc03e24f779434178a73a0b4bad62ed'
+pool = '0xcbcdf9626bc03e24f779434178a73a0b4bad62ed'
 
 history_df, pool = get_price_history(pool)
 
@@ -15,31 +16,69 @@ history_df['hourly_ret'] = history_df['close'].iloc[::-1].pct_change()
 
 daily_std = history_df['hourly_ret'].std()*24**0.5
 
-plot = history_df['hourly_ret'].hist(bins = 100)
+#plot = history_df['hourly_ret'].hist(bins = 100)
 
 history_df = history_df.dropna(subset=['hourly_ret'])
 
 #bootstrap
-price_paths = 10
+nprice_paths = 10000
 path_length = 720 #hours
-bootstrap =np.zeros(price_paths, dtype= object)
-bootstrap_result =np.zeros(price_paths, dtype= object)
+bootstrap =np.zeros([nprice_paths,path_length])
+price_path =np.zeros([nprice_paths,path_length])
 f = lambda x: x*random.choice([-1, 1])
 
 for i in range(len(bootstrap)):
     bootstrap[i] = history_df['hourly_ret'].sample(n = path_length, replace = True).abs().map(f).values
     bootstrap[i] = bootstrap[i] + 1
     bootstrap[i][0] = pool['token0']['price']
-    bootstrap_result[i] = np.cumprod(bootstrap[i])
+    price_path[i] = list(np.cumprod(bootstrap[i]))
 
 
 #print(bootstrap[0], bootstrap_result[0])
-for i in range(len(bootstrap_result)):
-    plt.plot(bootstrap_result[i])
-plt.ylim(min(min(x) for x in bootstrap_result)-0.5*max(max(x) for x in bootstrap_result),max(max(x) for x in bootstrap_result)*1.5)
+#for i in range(len(bootstrap_result)):
+#    plt.plot(bootstrap_result[i])
+#plt.ylim(min(min(x) for x in bootstrap_result)-0.5*max(max(x) for x in bootstrap_result),max(max(x) for x in bootstrap_result)*1.5)
+#plt.show()
+
+
+#calculate percentage of time 
+current_price = pool['token0']['price']
+spread = 0.08
+
+#metric 1: percentage of pools in range in given hour
+metric1 = []
+for i in range(path_length):
+    temp = price_path[:,i]
+    metric1.append(np.count_nonzero(temp[np.logical_and(temp > current_price*1/(1+spread), temp < current_price*(1+spread))])/nprice_paths)
+
+fig,axs = plt.subplots(3)
+plt.title(f'spread = {spread}')
+axs[0].plot(metric1)
+
+#metric 2: histogram of percentage of time each pool is in range
+metric2 = []
+for i in range(nprice_paths):
+    temp = price_path[i,:]
+    metric2.append(np.count_nonzero(temp[np.logical_and(temp > current_price*1/(1+spread), temp < current_price*(1+spread))])/path_length)
+
+axs[1].hist(metric2,bins=100)
+
+#metric 3: avg/median, min time to exit range
+metric3 = []
+
+for path in range(nprice_paths):
+    metric3.append(next((i for i,value in enumerate(price_path[path,:]) if (value < current_price*1/(1+spread) or value > current_price*(1+spread))),720))
+
+print(np.mean(metric3), np.min(metric3), np.median(metric3))
+
+axs[2].hist(metric3, bins=100)
+
 plt.show()
 
+# calculate expected gains for given spread and given capital level
 
+# calculate losses for each end price (relative to cash, half/half, all eth start)
 
+# calculate expected gain
 
-
+# figure out how the risk metrics fit in?
